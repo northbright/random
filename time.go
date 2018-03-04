643cb9a9d8e69d007580f5da5bfec0100725d194
Frank Xu <frank.xu@northbright.com>
2018-03-04 17:20:20 +0800

    Use time.Now().UnixNano() as random seed

--- time.go
+++ time.go
@@ -26,6 +26,7 @@ func RandTime(minTime, maxTime time.Time, pad time.Duration) (time.Time, error) 
 		return time.Now(), nil
 	}
 
+	rand.Seed(time.Now().UnixNano())
 	x := rand.Int63n(nMillisecond)
 
 	return min.Add(time.Duration(x) * time.Millisecond), nil
