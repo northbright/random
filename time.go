5d8f7a7d1e6192260a746cd4c55780997837fcb3
Frank Xu <frank.xu@northbright.com>
2018-03-05 17:28:24 +0800

    Rename RandTime to Time and remove pad argument

--- time.go
+++ time.go
@@ -6,28 +6,21 @@ import (
 	"time"
 )
 
-// RandTime returns random time between minimize time + pad and maximum time - pad.
-func RandTime(minTime, maxTime time.Time, pad time.Duration) (time.Time, error) {
+// Time returns random time between minimize time and maximum time.
+func Time(minTime, maxTime time.Time) (time.Time, error) {
 	if minTime.After(maxTime) {
 		return time.Now(), fmt.Errorf("min time > max time")
 	}
 
-	min := minTime.Add(pad)
-	max := maxTime.Add(0 - pad)
-
-	if min.After(max) {
-		return time.Now(), fmt.Errorf("min time + pad > max time - pad")
-	}
-
-	d := max.Sub(min)
+	d := maxTime.Sub(minTime)
 
 	nMillisecond := int64(d / time.Millisecond)
 	if nMillisecond <= 0 {
-		return min, nil
+		return minTime, nil
 	}
 
 	rand.Seed(time.Now().UnixNano())
 	x := rand.Int63n(nMillisecond)
 
-	return min.Add(time.Duration(x) * time.Millisecond), nil
+	return minTime.Add(time.Duration(x) * time.Millisecond), nil
 }
